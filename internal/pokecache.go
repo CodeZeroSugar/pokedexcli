ff08bf5aa1b53147ab84fcbd51582a941c1ef803
CodeZeroSugar <icvelder@gmail.com>
2025-12-12 00:23:05 -0600

    added cache struct and methods

---
+++ internal/pokecache.go
@@ -0,0 +1,60 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries  map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entries[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, exists := c.entries[key]
+	if exists {
+		return entry.val, true
+	}
+	return nil, false
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+	for range ticker.C {
+		c.mu.Lock()
+		for key, value := range c.entries {
+			if time.Since(value.createdAt) > c.interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go c.reapLoop()
+
+	return c
+}
